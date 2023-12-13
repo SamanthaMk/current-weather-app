54920919e2ed3770639d0fc2fc1db59f3a7930c3
Siphosethu Samantha Mkandwana <132946841+SamanthaMk@users.noreply.github.com>
2023-12-13 19:27:27 +0200

    I added bootstrap to my react app

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import React from 'react';
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather React App</h1>
-      <footer>
-        This project is coded by Siphosethu Samantha  is{" "}
-        <a
-          href="https://github.com/SamanthaMk/current-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on github
-        </a>
-      </footer>
+      <div className='container'>
+        <h1>Weather React App</h1>
+        <footer>
+          This project is coded by Siphosethu Samantha is{" "}
+          <a
+            href="https://github.com/SamanthaMk/current-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
