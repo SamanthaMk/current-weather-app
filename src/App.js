1a93a81a56b9e0efe79c2c7015b7f945409f9373
Siphosethu Samantha Mkandwana <132946841+SamanthaMk@users.noreply.github.com>
2023-12-13 16:50:35 +0200

    Updated App.js I added a link to Github  for my opened sourced code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import './App.css';
+import React from 'react';
 
 export default function App() {
-  return <div className="App">Weather React App</div>;
+  return (
+    <div className="App">
+      <h1>Weather React App</h1>
+      <footer>
+        This project is coded by Siphosethu Samantha  is{" "}
+        <a
+          href="https://github.com/SamanthaMk/current-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on github
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 
