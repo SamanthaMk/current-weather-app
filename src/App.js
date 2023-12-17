d55cb1a1c8f89cdb776b7a68d9e8e9108695eeec
Siphosethu Samantha Mkandwana <132946841+SamanthaMk@users.noreply.github.com>
2023-12-17 03:44:40 +0200

    I updated my weather App by adding an Api intergration into the app iI also changed my background color to navy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Weather from './Weather'
+import Weather from './Weather.js'
 import './App.css'
 
 export default function App() {
   return (
     <div className="App">
       <div className='container'>
-        <Weather />
+        <Weather defaultCity="London"/>
         <footer>
           This project is coded by Siphosethu Samantha is{" "}
           <a
--- src/Weather.js
+++ src/Weather.js
@@ -1,43 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter your City..."
-              className="form-control"
-              autoFocus="on"
-            />
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      temperature: response.data.temperature.current,
+      wind: response.data.wind.speed,
+      humidity: response.data.temperature.humidity,
+      city: response.data.city,
+      description: response.data.condition.description,
+      icon: "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-night.png",
+      index: response.data.condition.icon,
+      date:"Wensday 19:57"
+    });
+
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter your City..."
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input type="submit" value="Search" />
+            </div>
           </div>
-          <div className="col-3">
-            <input type="submit" value="Search" />
-          </div>
-        </div>
-      </form>
-      <div className="middle">
-        <h2>Midrand</h2>
-        <ul>
-          <li>Wensday 19:57 </li>
-          <li> Partly Cloudy</li>
-        </ul>
-        <div className="row">
-          <div className="col-6">
-            <img src="#" alt="partly cloudly" />
-            <span className="temp"> 27 °C</span>
-          </div>
-          <div className="col-6">
-            <ul>
-              <li className="humid">Humidity: 80%</li>
-              <li id="wind">Wind: 1 km/h</li>
-            </ul>
+        </form>
+        <div className="middle">
+          <h2>{weatherData.city}</h2>
+          <ul>
+            <li>{weatherData.date}</li>
+            <li> {weatherData.description}</li>
+          </ul>
+          <div className="row">
+            <div className="col-6">
+              <img src={weatherData.icon} alt={weatherData.index} />
+              <span className="temp">
+                {Math.round(weatherData.temperature)}°C
+              </span>
+            </div>
+            <div className="col-6">
+              <ul>
+                <li className="humid">Humidity:{weatherData.humidity} %</li>
+                <li id="wind">Wind: {weatherData.wind} km/h</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "b19t9a07a57a44df163do01147f91d11";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading forecast..";
+  }
 }
