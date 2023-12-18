e503c8c95395a9bdc2699b1b15f51a517d294373
Siphosethu Samantha Mkandwana <132946841+SamanthaMk@users.noreply.github.com>
2023-12-18 02:30:57 +0200

    I removed the link to my Api icon Url and just used the response method

--- src/Weather.js
+++ src/Weather.js
@@ -15,7 +15,7 @@ export default function Weather(props) {
       humidity: response.data.temperature.humidity,
       city: response.data.city,
       description: response.data.condition.description,
-      icon: "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-night.png",
+      icon: response.data.condition.icon_url,
       index: response.data.condition.icon,
       date: new Date(response.data.time * 1000),
     });
