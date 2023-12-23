248212cea2e3dd920fd3fa8400f0905f98dff88a
Siphosethu Samantha Mkandwana <132946841+SamanthaMk@users.noreply.github.com>
2023-12-23 18:12:24 +0200

    I Adde an Api url that will show my daily forecast data i also added new variables for my longitude and latitude data

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,10 +1,22 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon.js";
 import "./WeatherForecast.css";
+import axios from "axios";
 
+export default function WeatherForecast(props) {
+  function handleResponse(response) 
+  {
+    console.log(response.data)
+  }
+  console.log(props);
 
-export default function WeatherForecast() {
-  return (
+  let longitude = props.coordinates.longitude;
+  let latitude = props.coordinates.latitude;
+  let apiUrl =
+    `https://api.shecodes.io/weather/v1/forecast?lat=${latitude}&lon=${longitude}&key=b19t9a07a57a44df163do01147f91d11&units=metric`;
+  
+    axios.get(apiUrl).then(handleResponse);
+    return (
     <div className="weatherforecast">
       <div className="row">
         <div className="col">
