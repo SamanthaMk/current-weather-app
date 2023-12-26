5ed81e768c4c0a51f7d423a3cb3f25714fe7f222
Siphosethu Samantha Mkandwana <132946841+SamanthaMk@users.noreply.github.com>
2023-12-26 22:51:03 +0200

    Updated my code by adding a new component  that will handle the data of the forecast i also fetched the data for each day from my Api

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,17 +1,33 @@
 import React from "react";
 
-export default function WeatherForecastDay(props){
-   
-    return (
-      <div>
-     <div className="daily">{props.data.time}</div>
-  <img src={props.data.condition.icon_url} alt="sunny day" />
-        <div className="forecast-temp">
-          <span className="temp-max">15°</span>
-          <span className="temp-min">
-            | 10 °
-          </span>
-        </div>
+export default function WeatherForecastDay(props) {
+  function maxTemperature() {
+    let temperature = Math.round(props.data.temperature.maximum);
+    return `${temperature}°`;
+  }
+
+  function minTemperature() {
+    let temperature = Math.round(props.data.temperature.minimum);
+    return `${temperature}°`;
+  }
+
+  function day() {
+    let date = new Date(props.data.time * 1000);
+    let day = date.getDay();
+
+    let days = ["Sun", "Mon", "Tue", "Wen", "Thu", "Fri", "Sat"];
+
+    return days[day];
+  }
+
+  return (
+    <div>
+      <div className="daily">{day()}</div>
+      <img src={props.data.condition.icon_url} alt="sunny day" width="73" />
+      <div className="forecast-temp">
+        <span className="temp-max">{maxTemperature()}</span>
+        <span className="temp-min"> | {minTemperature()}</span>
       </div>
-    );
+    </div>
+  );
 }
